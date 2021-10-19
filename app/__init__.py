56019cc60eb8e812a247615ebc364976660fa434
Becca Elenzil <becca@adadevelopersacademy.org>
2021-10-19 10:40:18 -0700

    Refactor to separate route files

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,10 @@ def create_app():
     # __name__ store the name of the module we're in
     app = Flask(__name__)
 
-    from .routes import dog_bp
+    from .routes.dog_routes import dog_bp
     app.register_blueprint(dog_bp)
 
-    from .routes import cat_bp
+    from .routes.cat_routes import cat_bp
     app.register_blueprint(cat_bp)
 
     return app
