9fb07a153dbb145db382f7afaf1e1c31a809dc1c
Becca Elenzil <becca@adadevelopersacademy.org>
2021-10-19 10:32:53 -0700

    Get one cat with error handling

--- app/routes.py
+++ app/routes.py
@@ -49,6 +49,20 @@ def handle_cats():
     cats_response = [vars(cat) for cat in cats]
     return jsonify(cats_response)
 
+# get one cat
+@cat_bp.route("/<cat_id>", methods=["GET"])
+def handle_cat(cat_id):
+    try:
+        cat_id = int(cat_id)
+    except:
+        return "Bad data", 400
+
+    for cat in cats:
+        if cat.id == cat_id:
+            return vars(cat)
+            
+    return "Not found", 404
+
 # get all dogs
 @dog_bp.route("", methods=["GET"])
 def handle_dogs():
